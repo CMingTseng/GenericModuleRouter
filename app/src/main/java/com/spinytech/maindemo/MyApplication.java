e4a5d0e9f541334379f46dd443e144036ecba908
Guo,Feng(RIM) <guofeng05@baidu.com>
2018-07-06 19:54:04 +0800

    动态编译组件，独立运行

--- app/src/main/java/com/spinytech/maindemo/MyApplication.java
+++ app/src/main/java/com/spinytech/maindemo/MyApplication.java
@@ -1,24 +1,42 @@
 package com.spinytech.maindemo;
 
-import com.spinytech.macore.RouterManager;
-import com.spinytech.musicdemo.MusicApplicationLogic;
-import com.spinytech.picdemo.PicApplicationLogic;
-import com.spinytech.webdemo.WebApplicationLogic;
-
 import android.app.Application;
+import android.util.Pair;
+
+import com.spinytech.macore.BaseApplicationLogic;
+import com.spinytech.macore.RouterManager;
+
+import java.util.LinkedList;
 
 /**
  * Created by wanglei on 2016/11/29.
  */
 
 public class MyApplication extends Application{
+
+    public static LinkedList<Pair<String,String>> applicationLogic;
+
+    static {
+        applicationLogic = new LinkedList<>();
+        applicationLogic.add(new Pair<String, String>("com.spinytech.maindemo", "com.spinytech.maindemo.MainApplicationLogic"));
+        applicationLogic.add(new Pair<String, String>("com.spinytech.maindemo", "com.spinytech.webdemo.WebApplicationLogic"));
+        applicationLogic.add(new Pair<String, String>("com.spinytech.maindemo:music", "com.spinytech.musicdemo.MusicApplicationLogic"));
+        applicationLogic.add(new Pair<String, String>("com.spinytech.maindemo:pic", "com.spinytech.picdemo.PicApplicationLogic"));
+
+    }
+
     @Override
     public void onCreate() {
         super.onCreate();
-        RouterManager.getInstance().registerApplicationLogic(MainApplicationLogic.class);
-        RouterManager.getInstance().registerApplicationLogic(WebApplicationLogic.class);
-        RouterManager.getInstance().registerApplicationLogic(MusicApplicationLogic.class);
-        RouterManager.getInstance().registerApplicationLogic(PicApplicationLogic.class);
+
+        for (Pair<String, String> processLogic : applicationLogic) {
+            try {
+                Class<? extends BaseApplicationLogic> applicationLogicClass = (Class<? extends BaseApplicationLogic>) Class.forName(processLogic.second);
+                RouterManager.getInstance().registerApplicationLogic(applicationLogicClass);
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
         RouterManager.getInstance().init(this);
     }
 
